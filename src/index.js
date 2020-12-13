9224172bf4c911aa6b69ee3295c2b7e07236de21
juliana <63176299+juli4ict@users.noreply.github.com>
2020-12-13 22:52:27 +0800

    Added link

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
 
 function SearchEngine() {
-  return <App />;
-}
+  return ( <div>
+     <App />;
 
+
+    <a href="https://github.com/JulianaEkemezie/react-project" target="_blank">Coded by Juliana Ekemezie</a>
+  </div>)
+}
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
